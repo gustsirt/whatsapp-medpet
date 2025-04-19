eaaed944ff0b112387c5ed51c06cef2609476d36
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-19 20:01:14 -0300

    🆙 update (service): Handler - sendWelcome message update

--- app/modules/whatsapp/logic/messageHandler.js
+++ app/modules/whatsapp/logic/messageHandler.js
@@ -9,7 +9,7 @@ class MessageHandler {
       const incomingMessage = message.text.body.toLowerCase().trim(); // limpia el mensaje
 
       if (this.isGreeting(incomingMessage)) { // es saludo de apertura ??
-        await this.sendWelcomeMessage(message.from, message.id) // manda bienvenida
+        await this.sendWelcomeMessage(message.from, message.id, senderInfo) // manda bienvenida
       } else {
         const response = `Echo: ${message.text.body}`;
         await service.sendMessage(message.from, response, message.id); // manda mensaje ECHO
@@ -19,6 +19,16 @@ class MessageHandler {
     }
   }
 
+  // Obtiene nombre
+  getSenderName(senderInfo) {
+    // console.log("senderInfo: ", senderInfo); // { profile: { name: 'Gustavo Andrés' }, wa_id: '5493541xxxxxx' }
+
+    const name = senderInfo.profile?.name ? senderInfo.profile?.name.split(" ")[0] : null;
+
+    const sendName = name || senderInfo.wa_id || "";
+    return sendName == "" ? "" : " " + sendName
+  }
+
   // Si es saludo de apertura
   isGreeting(message) {
     const greetings = ["hola", "holas", "buenas", "buenas tardes", "buenas días"];
@@ -26,9 +36,9 @@ class MessageHandler {
   }
 
   // Mensaje de bienvenida
-  async sendWelcomeMessage(to, messageId) {
-    const welcomeMessage = "Hola, Bienvenido a nuestro servicio de Veterinaria online." +
-      "¿En Qué puedo ayudarte Hoy?";
+  async sendWelcomeMessage(to, messageId, senderInfo) {
+    const name = this.getSenderName(senderInfo);
+    const welcomeMessage = `Hola${name}, Bienvenido a MEDPET, Tu tienda de mascotas en línea. ¿En qué puedo ayudarte hoy?`;
     await service.sendMessage(to, welcomeMessage, messageId);
   }
 }
