f4596ec0aac0ae343dba90ea96b14188b6c48a16
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-19 17:50:26 -0300

    🆙 update (service): Handler - isGreeting

--- app/modules/whatsapp/logic/messageHandler.js
+++ app/modules/whatsapp/logic/messageHandler.js
@@ -1,13 +1,24 @@
 import service from "./service.js";
 
 class MessageHandler {
-  async handleIncomingMessage(message) {
+
+  // Recibe Mensaje
+  async handleIncomingMessage(message, senderInfo) {
     if (message?.type === 'text') {
       const response = `Echo: ${message.text.body}`;
-      await service.sendMessage(message.from, response, message.id);
-      await service.markAsRead(message.id);
+      await service.sendMessage(message.from, response, message.id); // mandamensaje
+      await service.markAsRead(message.id); // marca como leido
     }
   }
+
+  // Si es saludo de apertura
+  isGreeting(message) {
+    const greetings = ["hola", "buenas", "buenas tardes", "buenas días"];
+    const cleanMessage = message.toLowerCase().trim()
+    return greetings.includes(cleanMessage);
+  }
+
+
 }
 
 export default new MessageHandler();
