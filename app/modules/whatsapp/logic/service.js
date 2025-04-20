bff200f824a01b1daf04ab5aea7450eb5c42a0c8
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-20 18:46:24 -0300

    🆙 update (service): Service - sendIntereactiveButtonds

--- app/modules/whatsapp/logic/service.js
+++ app/modules/whatsapp/logic/service.js
@@ -11,14 +11,37 @@ const {
 class Service {
   constructor() { }
 
-  /* Manda un mensaje: ""
-      to: a quien
-      body: mensaje
-      messageId: indica que "responde" a este mensaje
+  // Doc Mensajes: https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
+
+  /** Manda mensaje como leido:
+   *    messageId: indica que mensaje es
+  */
+  async markAsRead(messageId) {
+    try {
+      await axios({
+        method: 'POST',
+        url: `https://graph.facebook.com/${API_VERSION}/${BUSINESS_PHONE}/messages`,
+        headers: {
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+        data: {
+          messaging_product: 'whatsapp',
+          status: 'read',
+          message_id: messageId,
+        },
+      });
+    } catch (error) {
+      console.error('Error marking message as read:', error);
+    }
+  }
+
+  /** Manda un mensaje
+   *   to: a quien
+   *   body: mensaje
+   *   messageId: indica que "responde" a este mensaje
   */
   async sendMessage(to, body, messageId) {
     try {
-      // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
       await axios({
         method: "POST",
         url: `https://graph.facebook.com/${API_VERSION}/${BUSINESS_PHONE}/messages`,
@@ -40,22 +63,36 @@ class Service {
     }
   }
 
-  async markAsRead(messageId) {
+  /** Manda mensaje con botones apra seleccionar
+   *   to: a quien
+   *   menuTitle: titulo de menu
+   *   buttons: botone
+   *    ejemplo: [{ type: 'reply', reply: { id: 'option_1', title: 'Agendar' } },]
+  */
+  async sendIntereactiveButtonds(to, menuTitle, buttons) {
     try {
       await axios({
-        method: 'POST',
+        method: "POST",
         url: `https://graph.facebook.com/${API_VERSION}/${BUSINESS_PHONE}/messages`,
         headers: {
           Authorization: `Bearer ${API_TOKEN}`,
         },
         data: {
-          messaging_product: 'whatsapp',
-          status: 'read',
-          message_id: messageId,
+          messaging_product: "whatsapp",
+          // to: to, // version original que daba error
+          to: to.replace(/^549/, "54"), // Corrige el formato si es necesario
+          type: "interactive",
+          interactive: {
+            type: "button",
+            body: { text: menuTitle },
+            action: {
+              buttons: buttons
+            }
+          }
         },
       });
     } catch (error) {
-      console.error('Error marking message as read:', error);
+      console.error('Error sending Buttons:', error);
     }
   }
 }
