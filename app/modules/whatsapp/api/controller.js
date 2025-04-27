4c1f1e3c5932572de01bfb5becfe50a7e2a66f51
Gustavo Sirtori <gustavo.sirtori@gmail.com>
2025-04-27 18:59:11 -0300

    🆙 update (controller): use normalizePhoneNumber

--- app/modules/whatsapp/api/controller.js
+++ app/modules/whatsapp/api/controller.js
@@ -1,20 +1,18 @@
 import service from "../logic/service.js";
 import messageHandler from "../logic/messageHandler.js";
 import configEnv from "../../../config/env.js";
+import { normalizePhoneNumber } from "../../../pkg/utils/normalizePhoneNumber.js";
 
 class Controller {
   constructor() { }
 
   // Recibe mensaje
   async handleIncoming(req, res) { // POST
-    // console.log("paso por aqui");
-
-    // console.log(req.body.entry?.[0]?.changes[0]?.value); // datos
-
     const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
     const senderInfo = req.body.entry?.[0]?.changes[0]?.value?.contacts?.[0]; // Quien lo envio
 
     if (message) {
+      message.from = normalizePhoneNumber(message.from)
       await messageHandler.handleIncomingMessage(message, senderInfo);
     }
     res.sendStatus(200);
@@ -22,14 +20,13 @@ class Controller {
 
   // Se usa para verificar que funcione esta conexión
   verifyWebhook(req, res) { // GET
-    // console.log("paso por aqui, verificando ");
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
     if (mode === 'subscribe' && token === configEnv.WEBHOOK_VERIFY_TOKEN) {
-      res.status(200).send(challenge);
       console.log('Webhook verified successfully!');
+      res.status(200).send(challenge);
     } else {
       res.sendStatus(403);
     }
